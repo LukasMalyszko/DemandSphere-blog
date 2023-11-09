57df9cd8ecb3dced015cad6cd1b0e1f8b35fb5f0
LukasMalyszko <lukas.malyszko@gmail.com~>
2023-11-09 14:32:53 +0100

    fix jsons

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,14 +1,14 @@
-require "net/http"
-require "uri"
-require "json"
-require "rest-client"
+require 'net/http'
+require 'uri'
+require 'json'
+require 'rest-client'
 require 'nokogiri'
-require "./.api_key.rb"
+
 
 class AuthController < ApplicationController
   def index
     if session[:user_id]
-      redirect_to action: "home"
+      redirect_to action: 'home'
     end
 
   end
@@ -18,18 +18,16 @@ class AuthController < ApplicationController
     password = params[:password]
 
     uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=
-              #{$api_key}")
+              #{ENV['API_KEY']}")
 
     res = Net::HTTP.post_form(
       uri,
-      "email" => email,
-      "password" => password
-      )
-
-    data = JSON.parse(res.body)
+      'email' => email,
+      'password' => password
+    )
 
     if res.is_a?(Net::HTTPSuccess)
-      redirect_to action: "index"
+      redirect_to action: 'index'
     end
   end
 
@@ -38,63 +36,63 @@ class AuthController < ApplicationController
     password = params[:password]
 
     uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=
-              #{$api_key}")
+      #{ENV['API_KEY']}")
 
     res = Net::HTTP.post_form(
       uri,
-      "email" => email,
-      "password" => password
-      )
+      'email' => email,
+      'password' => password
+    )
 
     data = JSON.parse(res.body)
 
     if res.is_a?(Net::HTTPSuccess)
-      session[:user_id] = data["localId"]
+      session[:user_id] = data['localId']
 
-      redirect_to action: "home"
+      redirect_to action: 'home'
     end
   end
 
   def logout
     session.clear
-    redirect_to action: "index"
+    redirect_to action: 'index'
   end
 
   def fetch_articles(num)
-  url = "https://www.demandsphere.com/feed/"
-  response = RestClient.get(url)
-  xml = Nokogiri::XML(response.body)
+    url = 'https://www.demandsphere.com/feed/'
+    response = RestClient.get(url)
+    xml = Nokogiri::XML(response.body)
 
-  articles = []
+    articles = []
 
-  xml.xpath('//item').each do |item|
-    title = item.at('title').text
-    link = item.at('link').text
-    creator = item.at('dc|creator',
-    'dc' => 'http://purl.org/dc/elements/1.1/').text
-    pub_date = Date.parse(item.at('pubDate').text).strftime("%Y-%m-%d")
-    description = item.at('description').inner_html
+    xml.xpath('//item').each do |item|
 
-    articles << {
-      title: title,
-      link: link,
-      creator: creator,
-      pub_date: pub_date,
-      description: description
-    }
+      title = item.at('title').text
+      link = item.at('link').text
+      creator = item.at('dc|creator',
+        'dc' => 'http://purl.org/dc/elements/1.1/').text
+      pub_date = Date.parse(item.at('pubDate').text).strftime('%Y-%m-%d')
+      description = item.at('description').inner_html
+
+      articles << {
+        title: title,
+        link: link,
+        creator: creator,
+        pub_date: pub_date,
+        description: description
+      }
+    end
+
+    articles.first(num)
   end
-
-  articles.first(num).to_json
-end
-
 
   def home
 
     if session[:user_id]
-       @articles = fetch_articles(5)
+      @articles = fetch_articles(5)
 
     else
-      redirect_to action: "index"
+      redirect_to action: 'index'
     end
   end
 
---
+++ app/javascript/asyncLoader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Twój kod JavaScript
+    console.log('Strona została załadowana!');
+    // Pobierz artykuły lub wykonaj inne operacje asynchroniczne
+  });
+  
